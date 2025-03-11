e4dd93e82822d3d277e9ec2155151cdecfa78225
Abdul-Rahimm <k213951@nu.edu.pk>
2025-03-11 15:50:48 +0500

    creating basic endpoints

--- chp2/server.js
+++ chp2/server.js
@@ -4,9 +4,19 @@ const express = require("express");
 
 const app = express();
 
-const PORT = 8383;
-
-app.listen(PORT, () => console.log("Server is running"));
+const PORT = 3000;
 
 // why need nodemon?
 // dev tool - not used in production
+
+app.get("/", (req, res) => {
+  console.log("first get request");
+  res.sendStatus(200);
+});
+
+app.get("/hello", (req, res) => {
+  setTimeout(() => console.log("After 1 second"), 1000);
+  res.status(200).send("hello world");
+});
+
+app.listen(PORT, () => console.log("Server is running"));
