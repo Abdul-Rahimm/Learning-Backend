dd0d60f8c9cee7eadbaf8899ec0194dee81abe2d
Abdul-Rahimm <k213951@nu.edu.pk>
2025-03-11 16:03:51 +0500

    created a basic API endpoint

--- chp2/server.js
+++ chp2/server.js
@@ -9,14 +9,23 @@ const PORT = 3000;
 // why need nodemon?
 // dev tool - not used in production
 
+//website endpoints - return html
 app.get("/", (req, res) => {
-  console.log("first get request");
-  res.sendStatus(200);
+  res.send("<h1>HomePage</h1>");
 });
 
 app.get("/hello", (req, res) => {
-  setTimeout(() => console.log("After 1 second"), 1000);
-  res.status(200).send("hello world");
+  res.status(200).send("<h1>Hello world</h1>");
+});
+
+let data = {
+  name: "abdul rahim",
+  hehe: "learning",
+};
+
+// API endpoints - non visual (login)
+app.get("/api/data", (req, res) => {
+  res.send(data);
 });
 
 app.listen(PORT, () => console.log("Server is running"));
